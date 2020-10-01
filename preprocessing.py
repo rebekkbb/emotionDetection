aad2b37d6bdf3cf1a19bb7a58dbbeeb733e775d0
Rebekka Birkeland Brøyn <rebekkbb@ntnu.no>
2020-10-01 14:04:00 +0200

    denoise made as a function

--- preprocessing.py
+++ preprocessing.py
@@ -5,17 +5,14 @@ from skimage.restoration import denoise_wavelet
 from scipy.signal import savgol_filter
 import os
 
-datafile=np.loadtxt("data/data4/GSRvalues.txt",delimiter=',',skiprows=1)
-x=datafile[:,0]
-y=datafile[:,1]
-y_denoise = savgol_filter(y,101,2)
-arr_denoise = np.column_stack((x,y_denoise))
-print(arr_denoise)
+def denoiseSignal(clipNum):
+    datafile=np.loadtxt("data/data+"+clipNum+"/GSRvalues.txt",delimiter=',',skiprows=1)
+    x=datafile[:,0]
+    y=datafile[:,1]
+    y_denoise = savgol_filter(y,101,2)
+    arr_denoise = np.column_stack((x,y_denoise))
 
-os.remove('data/data4/GSR_denoised.txt')
-f=open('data/data4/GSR_denoised.txt','a')
-f.write("time,gsr\n")
-np.savetxt(f,arr_denoise,delimiter=',')
-plt.plot(x,y)
-plt.plot(x,y_denoise)
-plt.show()
+    os.remove('data/data'+clipNum+'/GSR_denoised.txt')
+    f=open('data/data'+clipNum+'/GSR_denoised.txt','a')
+    f.write("time,gsr\n")
+    np.savetxt(f,arr_denoise,delimiter=',')
