1920f3a926f1838e7cef52ebe36136a6b438613c
Rebekka Birkeland Brøyn <rebekkbb@ntnu.no>
2020-10-28 17:13:35 +0100

    correct human resistance

--- plot.py
+++ plot.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 datafile=np.loadtxt("data/data199/Arduinovalues.txt",delimiter=',',skiprows=1)
 x=datafile[:,0]
 y=datafile[:,1]
-y2=datafile[:,2]
 
+ynew=[]
 
+for i in y:
+    ynew.append(((1024+2*i)*10000)/(512-i))
 
-plt.plot(x,y2)
+plt.plot(x,ynew)
 
 
 plt.show()
