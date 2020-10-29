ef58159edb15443cba3455bea67416d3c6315f71
Rebekka Birkeland Brøyn <rebekkbb@ntnu.no>
2020-10-29 10:20:55 +0100

    name the perfect-data plots

--- plot.py
+++ plot.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-datafile=np.loadtxt("data/data199/Arduinovalues.txt",delimiter=',',skiprows=1)
+datafile=np.loadtxt("data/data199/values.txt",delimiter=',',skiprows=1)
 x=datafile[:,0]
 y=datafile[:,1]
+y2=datafile[:,2]
 
-ynew=[]
 
-for i in y:
-    ynew.append(((1024+2*i)*10000)/(512-i))
+"""for i in y:
+    ynew.append(((1024+2*i)*10000)/(512-i))"""
 
-plt.plot(x,ynew)
+plt.title("RR-intervals")
+plt.xlabel("time stamp(sec from 1997)")
+plt.ylabel("RR-interval(ms)")
+
+plt.plot(x,y2)
 
 
 plt.show()
