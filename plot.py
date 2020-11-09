5c8ef2830ae4a383afe51a829944f56b56dd981f
Rebekka Birkeland Brøyn <rebekkbb@ntnu.no>
2020-11-09 16:24:55 +0100

    add data

--- plot.py
+++ plot.py
@@ -1,21 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-files=[1,3,4,5,6,7]
-x=np.arange(0,30,1)
+files=[1,4,6,7,11,12,13,14,18,19,20]
+x=np.arange(0,33,1)
+
+matr=[]
 for i in files:
     ynew=[]
-    datafile=np.loadtxt("data/data"+str(i)+"99/Arduinovalues.txt",delimiter=',',skiprows=1)
-    y=datafile[:,1][:30]
-    y2=datafile[:,2][:30]
-    for i in y:
-        ynew.append(((1024+2*i)*10000)/(512-i))
-    plt.plot(x,ynew)
+    datafile=np.loadtxt("data/data"+str(i)+"99/values.txt",delimiter=',',skiprows=1)
+    y=datafile[:,1][:33]
+    y2=datafile[:,2][:33]
+    """for i in y:
+        ynew.append(((1024+2*i)*10000)/(512-i))"""
+    matr.append(y)
+meanlist=np.mean(matr,axis=0)
+
+datafunny=np.loadtxt("data/data2/values (9).txt",delimiter=',',skiprows=1)
+plt.plot(x,meanlist)
+plt.plot(x,datafunny[:,1][57:])
 
 
-plt.title("RR-intervals")
+plt.title("GSR")
 plt.xlabel("time stamp(sec from 1997)")
-plt.ylabel("RR-interval(ms)")
+plt.ylabel("GSR")
 
 
 
